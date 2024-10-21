5c072348d3903446f3b58adb986a9a837cf4643f
m-d-nabeel <asharnabeel137@gmail.com>
2024-10-21 16:27:46 +0530

    check gh-page workflow

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/TradeScope/",
   plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
